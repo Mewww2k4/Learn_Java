2ab12c07c6c8e9398b4786f35ab7925f96053991
Mewww2k4 <121652389+Mewww2k4@users.noreply.github.com>
2023-08-27 07:43:51 +0700

    update file

---
+++ Java/src/vowel.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class vowel {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap ki tu: ");
+        char c = sc.next().charAt(0);
+        sc.close();
+
+        boolean isVowel = "aeiouAEIOU".indexOf(c) != -1;
+
+        if (isVowel) {
+            System.out.println("La nguyen am");
+        } else {
+            System.out.println("La phu am");
+        }
+    }
+}
