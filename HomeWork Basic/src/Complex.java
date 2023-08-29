418a1ae6e6d92f73b8d634e45726fb0b465d10d1
Mewww2k4 <121652389+Mewww2k4@users.noreply.github.com>
2023-08-29 14:27:53 +0700

    update

---
+++ HomeWork Basic/src/Complex.java
@@ -0,0 +1,9 @@
+//Cong 2 so phuc
+import java.util.Scanner;
+public class Complex {
+  public static void main(String[] args)
+  {
+      Scanner sc = new Scanner(System.in);
+
+  }
+}
---
+++ HomeWork Basic/src/Elseif.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Elseif {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap so diem de kiem tra PASS or FAIL : ");
+        int point = sc.nextInt();
+        if(point < 50)
+            System.out.println("FAIL");
+        else if(point >= 50 && point < 60)
+        {
+            System.out.println("HOC SINH TRUNG BINH Kem!!!");
+        } else if (point >= 60 && point < 70) {
+            System.out.println("HOC SINH TRUNG BINH");
+        } else if (point >= 70 && point < 80){
+            System.out.println("HOC SINH KHA");
+        }else if (point >= 80 && point < 90) {
+            System.out.println("HOC SINH Gioi");
+        }
+        else if (point >= 90 && point < 100) {
+            System.out.println("HOC SINH XUAT SAC");
+        }
+        else {
+            System.out.println("GIA TRI KHONG HOP LE");
+        }
+    }
+}
