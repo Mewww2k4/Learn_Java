8af4191d503af3d396795e1be769876853edca11
Mewww2k4 <121652389+Mewww2k4@users.noreply.github.com>
2023-08-29 11:50:42 +0700

    update file homework

--- HomeWork Basic/src/vowel.java
+++ HomeWork Basic/src/vowel.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class vowel {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -15,4 +14,5 @@ public class vowel {
             System.out.println("La phu am");
         }
     }
+
 }
