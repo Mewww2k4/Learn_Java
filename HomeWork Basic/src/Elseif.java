acb2e02697b1790ef7fcd9750ac1b49018213ba6
Mewww2k4 <121652389+Mewww2k4@users.noreply.github.com>
2023-08-29 14:30:57 +0700

    update

--- HomeWork Basic/src/Elseif.java
+++ HomeWork Basic/src/Elseif.java
@@ -6,22 +6,19 @@ public class Elseif {
         Scanner sc = new Scanner(System.in);
         System.out.println("Nhap so diem de kiem tra PASS or FAIL : ");
         int point = sc.nextInt();
-        if(point < 50)
+        if (point < 50) {
             System.out.println("FAIL");
-        else if(point >= 50 && point < 60)
-        {
+        } else if (point < 60) {
             System.out.println("HOC SINH TRUNG BINH Kem!!!");
-        } else if (point >= 60 && point < 70) {
+        } else if (point < 70) {
             System.out.println("HOC SINH TRUNG BINH");
-        } else if (point >= 70 && point < 80){
+        } else if (point < 80) {
             System.out.println("HOC SINH KHA");
-        }else if (point >= 80 && point < 90) {
+        } else if (point < 90) {
             System.out.println("HOC SINH Gioi");
-        }
-        else if (point >= 90 && point < 100) {
+        } else if (point < 100) {
             System.out.println("HOC SINH XUAT SAC");
-        }
-        else {
+        } else {
             System.out.println("GIA TRI KHONG HOP LE");
         }
     }
