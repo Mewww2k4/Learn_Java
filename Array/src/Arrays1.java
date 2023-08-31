3b3454a7a0b391f39eb275c23bc636f3f281b3f4
Mewww2k4 <121652389+Mewww2k4@users.noreply.github.com>
2023-09-01 00:11:23 +0700

    update file

--- Array/src/Arrays1.java
+++ Array/src/Arrays1.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class Arrays1 {
+    public static int input(int[] a, int n)
+    {
+        System.out.print("Nhap mang 1 : ");
+        for (int i = 0 ; i <n ; i++)
+        {
+            a[i] = sc.nextInt();
+        }
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = 0; // n : so luong phan tu
+        a[i] = sc.nextInt();
+
+    }
 }
