6ba499503840d790ce5e3c1bd7a25408c87bab9f
Mewww2k4 <121652389+Mewww2k4@users.noreply.github.com>
2023-09-04 19:15:33 +0700

    up

---
+++ Review/src/DateTime.java
@@ -0,0 +1,12 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateTime {
+    public static void main(String[] args)
+    {
+        Date date = new Date();
+        String strDateFormat = "HH:mm:ss a";
+        SimpleDateFormat sdf = new SimpleDateFormat(strDateFormat);
+        System.out.println(sdf.format(date));
+    }
+}
