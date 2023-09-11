36b2cb094ea4dec93d6ecb8e9d3127157f92255e
Mewww2k4 <121652389+Mewww2k4@users.noreply.github.com>
2023-09-11 14:41:57 +0700

    update

--- Array/src/Arrays1.java
+++ Array/src/Arrays1.java
@@ -1,19 +1,78 @@
-import java.util.Scanner;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
 
+
+import java.util.Scanner;
+/**
+ *
+ * @author ADMIN
+ */
 public class Arrays1 {
-    public static int input(int[] a, int n)
+    static Scanner sc = new Scanner(System.in);
+    public static void input(int[]a, int n)
     {
-        System.out.print("Nhap mang 1 : ");
-        for (int i = 0 ; i <n ; i++)
+        System.out.print("Nhap mang : ");
+        for(int i = 0 ; i < n ; i++)
         {
+            System.out.print("a["+i+"] = " );
             a[i] = sc.nextInt();
         }
     }
-    public static void main(String[] args)
+    public static void output(int[] a, int n)
     {
+        for (int i = 0 ; i < n ; i++)
+        {
+            System.out.print(a[i] + "   ");
+        }
+    }
+    public static void arrange(int []a , int n) {
+        System.out.print("Mang sau khi sap xep tang: ");
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (a[i] > a[j]) {
+                    int temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+    }
+    public static void decrease(int[] a, int n)
+    {
+        System.out.print("Mang sau khi sap xep giam : ");
+        for (int i = 0 ; i < n- 1; i ++)
+        {
+            for( int j = i+ 1; j < n ; j++)
+            {
+                if(a[i]< a[j])
+                {
+                    int temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+//        int a = 3;
+//        int b = 2;
+//        int c = a+ b;
+//        System.out.println("x = " + c);
         Scanner sc = new Scanner(System.in);
-        int n = 0; // n : so luong phan tu
-        a[i] = sc.nextInt();
+        int[] a = new int[100];
+        System.out.println("Nhap n : ");
+        int n = sc.nextInt();
+        input(a,n);
+        System.out.print("Mang la : ");
+        output(a,n);
+        System.out.print("\n");
+        arrange(a,n);
+        output(a,n);
+        System.out.print("\n");
+        decrease(a, n);
+        output(a,n);
 
     }
 }
